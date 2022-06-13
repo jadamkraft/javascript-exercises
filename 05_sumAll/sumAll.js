b5264773fafc1c1ea6e3e158282887dad528a5d4
jadamkraft <jadamkraft@gmail.com>
2022-06-13 16:16:59 -0500

    Add loop to push array then forEach call to sum the numbers in the array. Add reverse loop to unshift if largest number comes first in arguments. Add if/else conditionals to catch non-numbers and negatives

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(first, last) {
+    let numArray = [];
+    let finalSum = 0;
+        if (typeof first !== 'number' || typeof last !== 'number') {
+            return 'ERROR';
+        } else if (first < 0 || last < 0) {
+            return 'ERROR';
+        } else if (first > last) {
+            for (i = first; i >= last; i--) {
+                numArray.unshift(i);
+            }
+        } else if (first < last) {
+            for (i = first; i <= last; i++) {
+                numArray.push(i);
+            }
+        }
+        numArray.forEach(function(num) {
+            finalSum += num; 
+        });
+        return finalSum;
 };
 
 // Do not edit below this line
