8893ddaf8ac4291a6bca4ab30d21b096f68d35d0
jadamkraft <jadamkraft@gmail.com>
2022-06-18 12:46:05 -0500

    Create while loops to solve power and factorial function challenges

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -14,16 +14,30 @@ const sum = function(arr) {
   return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let multiple = 1;
+  arr.forEach(num => {
+    multiple *= num;
+  });
+  return multiple;
 };
 
-const power = function() {
-	
+const power = function(num, exp) {
+  let starter = 1;
+  while (exp > 0) {
+    starter *= num;
+    exp--;
+  }
+  return starter;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let fact = 1;
+	while (num > 0) {
+    fact *= num;
+    num--;
+  }
+  return fact;
 };
 
 // Do not edit below this line
