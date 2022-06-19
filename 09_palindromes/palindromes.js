c88e84911d7c2a54e27a3e6fad9c1e089de81837
jadamkraft <jadamkraft@gmail.com>
2022-06-19 08:25:07 -0500

    Add fibonacci logic and test working

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
-
+let palArray = [];
+const palindromes = function (pal) {
+    pal = pal.toLowerCase();
+    pal = pal.replace(/[^\w\s']|_/g, "");
+    pal = pal.replace(/\s/g, "");
+    for (let i = 0; i < pal.length; i++) {
+    palArray.push(pal[i]);
+    };
+    let test = true;
+    let arrLength = palArray.length;
+    for (let j = 0; j < arrLength / 2; j++) {
+        if (palArray[j] !== palArray[arrLength - 1 - j]) {
+            test = false;
+        };
+    };
+    return test;
 };
 
 // Do not edit below this line
-module.exports = palindromes;
+//module.exports = palindromes;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let valLeft = 0;
+    let valRight = 1;
+    let newValue = 0;
+    for (let i = 0; i < num - 1; i++) {
+        newValue = valLeft + valRight;
+        valLeft = valRight;
+        valRight = newValue;
+    }
+    return valRight;
 };
 
 // Do not edit below this line
